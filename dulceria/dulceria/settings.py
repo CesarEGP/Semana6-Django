071dc1294753ed43c808170c265d24a531a34ebe
Cesar Garcia <cyberpgp@hotmail.com>
2023-09-03 22:41:56 -0500

    se actualizo requirements.txt

--- dulceria/dulceria/settings.py
+++ dulceria/dulceria/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'gitwhitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
